32f3e0e239dd7110355f04455e9cc0c04a31cd05
Jiayu Fu <j3fu@ucsd.edu>
2020-02-06 12:13:36 -0800

    finsih

--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,10 @@
 var data = require("../data.json");
 
 exports.addFriend = function(request, response) {    
-	// Your code goes here
- }
+	var a = request.query.name;
+	var b = request.query.description;
+	var c = {"name":a,"description":b,"imageURL":"http://lorempixel.com/400/400/people"};
+	console.log(c);
+	data.friends.push(c);
+	response.render('index',data);
+ };
